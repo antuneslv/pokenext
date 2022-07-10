5fec1ce1b37c645d06f0ff15c2831407535848cd
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-07-10 17:58:46 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,48 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Main, LinksContainer } from '../styles'
 
 const Home: NextPage = () => {
 
-  const genTwoPokemons = 251
-  const genThreePokemons = 386
-  const genFourPokemons = 493
-  const genFivePokemons = 649
-  const genSixPokemons = 721
-  const genSevenPokemons = 809
-  const genEightPokemons = 905
-
   return (
-    <div>
+    <>
       <Head>
         <link rel='icon' href='/images/favicon.ico' />
         <title>Poke Dex</title>
       </Head>
-      <h1>Poke Dex</h1>
-    </div>
+      <Main>
+        <h2>
+          Poké<span>Next</span>
+        </h2>
+        <LinksContainer>
+          <Link href='/gen-one'>
+            <a>Gen I</a>
+          </Link>
+          <Link href='/gen-two'>
+            <a>Gen II</a>
+          </Link>
+          <Link href='/gen-three'>
+            <a>Gen III</a>
+          </Link>
+          <Link href='/gen-four'>
+            <a>Gen IV</a>
+          </Link>
+          <Link href='/gen-five'>
+            <a>Gen V</a>
+          </Link>
+          <Link href='/gen-six'>
+            <a>Gen VI</a>
+          </Link>
+          <Link href='/gen-seven'>
+            <a>Gen VII</a>
+          </Link>
+          <Link href='/gen-eight'>
+            <a>Gen VIII</a>
+          </Link>
+        </LinksContainer>
+      </Main>
+    </>
   )
 }
 
---
+++ src/styles/index.ts
@@ -0,0 +1,50 @@
+import { styled } from '@stitches/react'
+
+export const Main = styled('main', {
+  maxWidth: '1200px',
+  margin: '0 auto',
+  padding: '40px',
+
+  h2: {
+    fontFamily: '"Pokemon Solid", sans-serif',
+    letterSpacing: '2px',
+    fontWeight: '200',
+    fontSize: '40px',
+    textAlign: 'center',
+
+    span: {
+      color: '#ff0000'
+    }
+  }
+})
+
+export const LinksContainer = styled('div', {
+  marginTop: '40px',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(260px, 1fr))',
+  gap: '40px',
+
+  '@media (min-width: 720px)': {
+    marginTop: '60px',
+    gap: '60px'
+  },
+  '@media (min-width: 1024px)': {
+    marginTop: '80px'
+  },
+
+  a: {
+    border: '1px solid #fff',
+    width: '100px',
+    height: '50px',
+    borderRadius: '40px',
+    textAlign: 'center',
+    lineHeight: '3',
+    textDecoration: 'none',
+    margin: '0 auto',
+    transition: 'all 0.3s',
+
+    '&:hover': {
+      backgroundColor: '#ff0000'
+    }
+  }
+})
