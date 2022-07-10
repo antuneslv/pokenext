247d6dbce869c4dc45fc426a1dbf6c64848fa407
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-07-10 20:08:52 -0300

    fix: search icon sometimes not loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,7 +27,7 @@ function Document() {
         />
         <link
           rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=optional'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=swap'
         />
       </Head>
       <body>
