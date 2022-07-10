3d13227ecd61aa102abf2d0c7928d230621d6773
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-07-10 15:32:00 -0300

    feat: add React Query Client Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from 'next/app'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '../../services/queryClient'
 import MainContainer from '../components/main-container'
 import { globalStyles } from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <MainContainer>
         <Component {...pageProps} />
       </MainContainer>
+    </QueryClientProvider>
       {globalStyles()}
     </>
   )
