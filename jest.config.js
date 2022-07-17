36bfc95f02d3e2dd43b0346c9b46b4796897f227
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-07-17 14:48:27 -0300

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next"],
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setup-tests.ts"],
+  testPathIgnorePatterns: ['/node_modules/', '/.next'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup-tests.ts'],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   testEnvironment: 'jsdom'
 }
